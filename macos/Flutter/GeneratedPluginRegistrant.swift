635cf62068462f9c2371425bc07161554f775fd9
Zaman Sheikh <zaman6545@gmail.com>
2025-06-13 12:01:08 +0600

    Update Firebase configuration and remove FFmpeg dependencies
    - Updated google-services.json with new project details for 'dlstarlive'.
    - Modified firebase.json to reflect changes in project ID and app ID.
    - Updated firebase_options.dart with new API keys and project details.
    - Commented out FFmpeg related code in video_editor_screen.dart and removed FFmpegKitFlutter plugin registration in macOS.
    - Updated gradle.properties to include Java path for Windows.
    - Removed unnecessary build files and updated pubspec.yaml to comment out ffmpeg_kit_flutter dependency.
    - Added .gitignore file to exclude build outputs and IDE specific files.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import agora_rtc_engine
 import cloud_functions
 import device_info_plus
 import emoji_picker_flutter
-import ffmpeg_kit_flutter
 import file_selector_macos
 import firebase_analytics
 import firebase_auth
@@ -36,7 +35,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
-  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
