d88110085b3af1200d9cedbaa2045a9f27395e85
Zaman Sheikh <zaman6545@gmail.com>
2025-06-14 11:56:34 +0600

    chore: comment out server initialization and debug print statements

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,10 @@ Future<void> main() async {
   }
 
   // Start the server on port 8080
-  final server = await shelf_io.serve(handler, InternetAddress.loopbackIPv4, 8080);
-  debugPrint('Serving at http://${server.address.host}:${server.port}');
+  // final server = await shelf_io.serve(handler, InternetAddress.loopbackIPv4, 8080);
+  // debugPrint('Serving at http://${server.address.host}:${server.port}');
+
+  //Load environment variables
   await dotenv.load(fileName: ".env");
 
   await Firebase.initializeApp(
