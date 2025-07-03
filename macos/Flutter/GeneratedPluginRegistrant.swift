427a343bf76079cbbb9fc32a2ad183870b63d045
Zaman Sheikh <zaman6545@gmail.com>
2025-07-03 20:18:37 +0600

    Add new assets and update dependencies for TeenPatty game
    - Added new image asset: TeenPatty V3.jpg
    - Added new WebAssembly file: ab12d88d9afba56143772d2bdab4b0d7.wasm
    - Updated index.html to reference new data URL and WebAssembly file
    - Changed WebSocket server port from 9000 to 9001
    - Updated GeneratedPluginRegistrant.swift to include file_picker plugin
    - Updated pubspec.lock to include new dependencies and updated versions:
      - Added cli_util (0.4.2)
      - Added file_picker (10.2.0)
      - Added flutter_launcher_icons (0.14.4)
      - Updated google_sign_in_android to version 6.2.1
      - Updated google_sign_in_ios to version 5.9.0
      - Updated logger to version 2.6.0

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import agora_rtc_engine
 import cloud_functions
 import device_info_plus
 import emoji_picker_flutter
+import file_picker
 import file_selector_macos
 import firebase_analytics
 import firebase_auth
@@ -35,6 +36,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
