7d4d9ea0125fb17d43d3e5c9810db335eab12272
Zaman Sheikh <zaman6545@gmail.com>
2025-08-18 20:25:51 +0600

    refactor: enhance GetRoomModel to include messages and membersDetails handling

--- lib/core/network/models/get_room_model.dart
+++ lib/core/network/models/get_room_model.dart
@@ -25,7 +25,9 @@ class GetRoomModel {
   final String hostId;
   final String roomId;
   final String roomType;
+  final List<dynamic> messages;
   final HostDetails? hostDetails;
+  final List<HostDetails> membersDetails;
   final List<String> members;
   final List<String> bannedUsers;
   final List<String> brodcasters;
@@ -38,6 +40,8 @@ class GetRoomModel {
     required this.roomId,
     required this.roomType,
     this.hostDetails,
+    this.messages = const [],
+    this.membersDetails = const [],
     required this.members,
     required this.bannedUsers,
     required this.brodcasters,
@@ -54,6 +58,10 @@ class GetRoomModel {
       hostDetails: json['hostDetails'] != null
           ? HostDetails.fromJson(json['hostDetails'])
           : null,
+      messages: List<dynamic>.from(json['messages'] ?? []),
+      membersDetails: (json['membersDetails'] as List<dynamic>? ?? [])
+          .map((e) => HostDetails.fromJson(e))
+          .toList(),
       members: List<String>.from(json['members'] ?? []),
       bannedUsers: List<String>.from(json['bannedUsers'] ?? []),
       brodcasters: List<String>.from(json['brodcasters'] ?? []),
