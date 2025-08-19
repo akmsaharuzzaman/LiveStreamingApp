8e8f7b68ebb76a68f31aec406dd90a822e57a83c
Zaman Sheikh <zaman6545@gmail.com>
2025-08-19 14:02:03 +0600

    feat: add AdminDetailsModel for managing admin details in the application

---
+++ lib/core/network/models/admin_details_model.dart
@@ -0,0 +1,25 @@
+class AdminDetailsModel {
+  final String name;
+  final String avatar;
+  final String uid;
+  final String id;
+  final String message;
+
+  AdminDetailsModel({
+    required this.name,
+    required this.avatar,
+    required this.uid,
+    required this.id,
+    required this.message,
+  });
+
+  factory AdminDetailsModel.fromJson(Map<String, dynamic> json) {
+    return AdminDetailsModel(
+      name: json['adminDetails']['name'],
+      avatar: json['adminDetails']['avatar'],
+      uid: json['adminDetails']['uid'],
+      id: json['_id'],
+      message: json['message'],
+    );
+  }
+}
